ed8abe27dc58f37c83c0ae2f7af934a3191a493a
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-08 14:36:16 -0700

    update name to username

--- app/poros/commit.rb
+++ app/poros/commit.rb
@@ -5,13 +5,13 @@ class Commit
   end
 
   def name
-    @username = commits[:author][:login]
+    @usernames = commits[:author][:login]
   end
 
   def count
     commit_count_hash = Hash.new(0)
-    @name.each do |name|
-      commit_count_hash[:name] += 1
+    @username.each do |user|
+      commit_count_hash[:user] += 1
     end
   end
 end
